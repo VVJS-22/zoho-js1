634b0afea20b8cd29276eab567fe5df292a843e4
VVJS-22 <s.jayeshchennai@gmail.com>
2023-03-21 01:19:03 +0530

    optimised@21-3-23

--- Calculator.js
+++ Calculator.js
@@ -1,90 +1,86 @@
 export class Calculator {
 
-    inputToDisplay(btn, display) {
+    static inputToDisplay(btn, display) {
         document.querySelector(".error").classList.add("hide");
-        const expression = () => {
-            const splitted = display.value.split(/([-+*^\/])/);
-            console.log(splitted)
-            let last = splitted[splitted.length - 1]
-            if (btn.innerText === "0") {
-                if (last.match(/^0$/)) {
-                    return false
-                }
+        const splitted = display.value.split(/([-+*^\/])/);
+        let last = splitted[splitted.length - 1]
+        if (btn.innerText === "0") {
+            if (last.match(/^0$/)) {
+                return false
             }
-            if (!["0", "."].includes(btn.innerText)) {
-                if (last.match(/^0$/)) {
-                    if (btn.innerText.match(/[1-9]/)) {
-                        splitted[splitted.length - 1] = btn.innerText
-                        return display.value = splitted.join("")
-                    }
-                }
-            }
-            if (btn.innerText === ".") {
-                if (last === "") {
-                    splitted[splitted.length - 1] = 0+btn.innerText
+        }
+        if (!["0", "."].includes(btn.innerText)) {
+            if (last.match(/^0$/)) {
+                if (btn.innerText.match(/[1-9]/)) {
+                    splitted[splitted.length - 1] = btn.innerText
                     return display.value = splitted.join("")
                 }
-                if (last.match(/\./)) {
-                    return false
-                }
             }
-            if (btn.innerText.match(/[-+*^\/]/)) {
-                if (last === "") {
-                    if (splitted[splitted.length - 2] === "-" && splitted[splitted.length - 3] === "" && splitted[splitted.length - 4].match(/[-+*^\/]/)) {
-                        splitted[splitted.length - 4] = btn.innerText
-                        const splitSlice = splitted.slice(0, splitted.length - 3);
-                        return display.value = splitSlice.join("");
-                    }
-                    if (display.value !== "" && display.value !== "-" && btn.innerText !== "-") {
-                        if (splitted[splitted.length - 2] === btn.innerText) {
-                            return false;
-                        }
-                        splitted[splitted.length - 2] = btn.innerText;
-                        return display.value = splitted.join("");
-                    } else if (btn.innerText !== "-") {
+        }
+        if (btn.innerText === ".") {
+            if (last === "") {
+                splitted[splitted.length - 1] = 0+btn.innerText
+                return display.value = splitted.join("")
+            }
+            if (last.match(/\./)) {
+                return false
+            }
+        }
+        if (btn.innerText.match(/[-+*^\/]/)) {
+            if (last === "") {
+                if (splitted[splitted.length - 2] === "-" && splitted[splitted.length - 3] === "" && splitted[splitted.length - 4].match(/[-+*^\/]/)) {
+                    splitted[splitted.length - 4] = btn.innerText
+                    const splitSlice = splitted.slice(0, splitted.length - 3);
+                    return display.value = splitSlice.join("");
+                }
+                if (display.value !== "" && display.value !== "-" && btn.innerText !== "-") {
+                    if (splitted[splitted.length - 2] === btn.innerText) {
                         return false;
                     }
-                }
-            }
-            if (btn.innerText === "-" && last === "") {
-                if (splitted[splitted.length - 2] === "-") {
-                    return false
-                }
-                if (splitted[splitted.length - 2] === "+") {
-                    splitted[splitted.length - 2] 
-                    = btn.innerText;
+                    splitted[splitted.length - 2] = btn.innerText;
                     return display.value = splitted.join("");
+                } else if (btn.innerText !== "-") {
+                    return false;
                 }
             }
-            if (last !== "") {
-                if ((btn.innerText.match(/[0-9]/) && last[last.length - 1] === ")") || ((btn.innerText === "(" && last[last.length - 1].match(/[0-9.]/)))) {
-                    return display.value += "*"+btn.innerText
-                }
-            }
-            display.value += btn.innerText
         }
-        expression();
+        if (btn.innerText === "-" && last === "") {
+            if (splitted[splitted.length - 2] === "-") {
+                return false
+            }
+            if (splitted[splitted.length - 2] === "+") {
+                splitted[splitted.length - 2] 
+                = btn.innerText;
+                return display.value = splitted.join("");
+            }
+        }
+        if (last !== "") {
+            if ((btn.innerText.match(/[0-9]/) && last[last.length - 1] === ")") || ((btn.innerText === "(" && last[last.length - 1].match(/[0-9.]/)))) {
+                return display.value += "*"+btn.innerText
+            }
+        }
+        display.value += btn.innerText;
     }
 
-    clear(display) {
+    static clear(display) {
         document.querySelector(".error").classList.add("hide");
         display.value = ""
     }
 
-    back(display) {
+    static back(display) {
         if (display.value) {
             display.value = display.value.substring(0, display.value.length - 1)
         }
     }
 
-    execute(display) {
+    static execute(display) {
         try {
             if (display.value !== "") {
                 let expression = display.value.replaceAll("^", "**")
-                display.value = new Function(`return ${expression}`)();
+                let result = new Function(`return ${expression}`)();
+                display.value = Number.isInteger(result) ? result : result.toFixed(2);
             }
         } catch (error) {
-            console.log(error)
             document.querySelector(".error").classList.remove("hide");
             return false
         }
--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 import { Calculator } from "./Calculator.js";
 
-const calculator = new Calculator()
-
-console.log(calculator)
-
 const buttons = Array.from(document.getElementsByTagName("button"))
 const display = document.querySelector(".display")
-const back = document.querySelector(".back")
 
 buttons.map(btn => {
     btn.addEventListener("click" , () => {
         switch (btn.innerText) {
             case "AC":
-                calculator.clear(display)
+                Calculator.clear(display)
                 break;
             case "=":
-                calculator.execute(display)
+                Calculator.execute(display)
+                break;
+            case "Back":
+                Calculator.back(display)
                 break;
             default:
-                calculator.inputToDisplay(btn, display)
+                Calculator.inputToDisplay(btn, display)
                 break;
         }
     })
 });
-
-back.addEventListener("click", () => {
-    calculator.back(display);
-})
-
 
 
